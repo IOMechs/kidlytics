b2780aa4e5beaa85da96094bbf2cb92617b13d08
Hashir Jamal <hashir.jamal@iomechs.com>
2025-07-14 18:06:47 +0500

    Router link

--- src/app/components/call-to-action/call-to-action.ts
+++ src/app/components/call-to-action/call-to-action.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-call-to-action',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './call-to-action.html',
-  styleUrl: './call-to-action.css'
+  styleUrl: './call-to-action.css',
 })
-export class CallToAction {
-
-}
+export class CallToAction {}
