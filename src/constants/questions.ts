f828b8cb33920a024db237f8509dcbf4c269b3c4
Hashir Jamal <hashir.jamal@iomechs.com>
2025-07-17 12:27:08 +0500

    New questions and placeholders

--- src/constants/questions.ts
+++ src/constants/questions.ts
@@ -3,11 +3,25 @@ export const STORY_QUESTIONS = [
     question: 'Who is this story for?',
     isMcq: false,
     options: [], // Freeform input: Name and Age
+    placeholder: 'e.g. Susan a 7 years old girl from Italy',
+  },
+  {
+    question: 'What is their age group ',
+    isMcq: true,
+    options: ['1-5 years', '6-10 years', '11-15 years', '15 and above'],
+    placeholder: '',
+  },
+  {
+    question: 'Which language would you like the story to be in',
+    isMcq: false,
+    options: [],
+    placeholder: 'e.g. English',
   },
   {
     question: 'What are their favorite things?',
     isMcq: false,
     options: [], // Freeform input: things they love
+    placeholder: 'e.g. Toys, books, animals',
   },
   {
     question: 'What kind of world should the story happen in?',
@@ -20,6 +34,7 @@ export const STORY_QUESTIONS = [
       '🏫 School',
       '🏡 Everyday life',
     ],
+    placeholder: '',
   },
   {
     question: 'What should the story teach or focus on?',
@@ -31,10 +46,12 @@ export const STORY_QUESTIONS = [
       '👫 Friendship',
       '✨ Just for fun',
     ],
+    placeholder: '',
   },
   {
     question: 'What mood should the story have?',
     isMcq: true,
     options: ['😂 Funny', '🧙 Magical', '😴 Calm', '🧗 Adventurous'],
+    placeholder: '',
   },
 ];
