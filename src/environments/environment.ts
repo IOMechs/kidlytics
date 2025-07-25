719de60570f0e11cfd12e31cc462aa12dc77ed31
Hashir Jamal <hashir.jamal@iomechs.com>
2025-07-25 12:24:49 +0500

    Sample environment files

---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: false,
+  apiUrl: '',
+  FRONTEND_BASE_URL: '',
+  apiKey: '',
+  authDomain: '',
+  projectId: '',
+  storageBucket: '',
+  messagingSenderId: '',
+  appId: '',
+  gcpProjectId: '',
+};
